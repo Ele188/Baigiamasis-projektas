109f3f195878912914e5719547dddd24888a0f3d
Ele188 <elena.jankauskaite1@gmail.com>
2024-02-18 19:25:42 +0200

    Added jwt

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import UserModel from "../models/user.js";
+import jwt from "jsonwebtoken"
 
 const SIGN_UP = async (req, res) => {
 
@@ -23,11 +24,35 @@ const SIGN_UP = async (req, res) => {
 };
 
 const LOGIN = async (req, res) => {
-    try{
-      const questions = await UserModel.find();
-      return res
-      .status(200)
-      .json({questions: questions});
+ try{
+   console.log(req.body.password)
+
+   const user = await UserModel.findOne({email:req.body.email});
+
+   if(!user){
+    return res.status(401).json({message: "Invalid login data"});
+   }
+
+   const isPasswordMatch = bcrypt.compareSync(
+    req.body.password, 
+    user.password
+    );
+
+    if(!isPasswordMatch){
+     return res.status(401).json({message: "Invalid login data"});
+    }
+
+    const token = jwt.sign(
+    { 
+     email: user.email, 
+     id: user._id
+    }, 
+     process.env.JWT_SECRET, 
+    {
+     expiresIn: "96h"
+    });
+
+ return res.status(201).json({ jwt: token });
     } catch (err){
     console.log(err);
     return res.status(500).json({message: "Soemthing went wrong"});
--- src/routes/question.js
+++ src/routes/question.js
@@ -1,19 +1,21 @@
 import express from "express";
 import {GET_ALL_QUESTIONS, ADD_QUESTION, GET_QUESTION_BY_USER_ID, DELETE_QUESTION_BY_ID} from "../controllers/question.js";
 
+import auth from "../middleware/auth.js"
+
 const router = express.Router();
 
-router.get('/questions', GET_ALL_QUESTIONS);
+router.get('/questions', auth, GET_ALL_QUESTIONS);
   
-  router.get('/question/:userId', GET_QUESTION_BY_USER_ID);
+  router.get('/question/:userId', auth, GET_QUESTION_BY_USER_ID);
   
-  router.post('/question', ADD_QUESTION);
+  router.post('/question',auth, ADD_QUESTION);
   
-  router.post('/question/:id/like', GET_ALL_QUESTIONS);
+  router.post('/question/:id/like',auth, GET_ALL_QUESTIONS);
   
-  router.post('/question/:id/dislike', GET_ALL_QUESTIONS);
+  router.post('/question/:id/dislike',auth, GET_ALL_QUESTIONS);
   
-  router.delete('/question/:id', DELETE_QUESTION_BY_ID);
+  router.delete('/question/:id',auth, DELETE_QUESTION_BY_ID);
   
 
   export default router; 
